b461e1979c5d5c5ef5b131629bc60ab26cf03634
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-12-21 01:44:55 -0300

    feat: Métodos e eventos de socket

--- src/server.js
+++ src/server.js
@@ -6,11 +6,34 @@ const { Server } = require("socket.io");
 const io = new Server(httpServer);
 const PORT = process.env.PORT || 3000;
 
+const { addUser, getCurrentUser, deleteUser } = require("./utils/users");
+
 io.on("connection", socket => {
-    console.log("Connected");
-    socket.on("chatMessage", msg => {
-        io.emit("chatMessage", msg);
+    socket.on("login", ({ username, room }) => {
+        const user = addUser(socket.id, username, room);
+
+        socket.join(user.room);
+
+        socket.emit("message", `Welcome ${user.username}!`);
+
+        socket.broadcast
+            .to(user.room)
+            .emit("message", `${user.username} joined the chat`);
+
+        socket.on("chatMessage", msg => {
+            const user = getCurrentUser(socket.id);
+            io.to(user.room).emit("message", msg);
+        });
+
+        socket.on("disconnect", () => {
+            const user = deleteUser(socket.id);
+
+            if (user)
+                io.to(user.room)
+                    .emit("message", `${user.username} left`);
+        });
     });
 });
 
-httpServer.listen(PORT, () => console.log(`Server listening on port:${PORT}`));
+httpServer.listen(PORT,
+    () => console.log(`Server listening on port:${PORT}`));
