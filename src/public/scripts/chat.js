29b6f2a153d2c0d520749e4edf07b0c8f167dc73
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-12-21 01:44:17 -0300

    feat: Adicionado eventos de socket

--- src/public/scripts/chat.js
+++ src/public/scripts/chat.js
@@ -4,6 +4,11 @@ const messages = document.getElementById("messages");
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 
+const username = document.getElementById("username").textContent;
+const room = document.getElementById("room").textContent;
+
+socket.emit("login", { username, room });
+
 form.addEventListener("submit", event => {
     event.preventDefault();
     if (input.value) {
@@ -12,7 +17,7 @@ form.addEventListener("submit", event => {
     };
 });
 
-socket.on("chatMessage", msg => {
+socket.on("message", msg => {
     const text = document.createElement("li");
     text.textContent = msg;
     messages.appendChild(text);
