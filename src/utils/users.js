5f129f640dfba074fca8ddd4a1f6ce25b100f1cf
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-12-21 01:43:34 -0300

    Initial commit

---
+++ src/utils/users.js
@@ -0,0 +1,22 @@
+const users = [];
+
+const addUser = (id, username, room) => {
+    const user = { id, username, room };
+    users.push(user);
+    return user;
+};
+
+const getCurrentUser = id => users.find(user => user.id === id)
+
+const deleteUser = id => {
+    const index = users.findIndex(user => user.id === id);
+
+    if (index !== -1)
+        return users.splice(index, 1)[0];
+};
+
+module.exports = {
+    addUser,
+    getCurrentUser,
+    deleteUser
+};
